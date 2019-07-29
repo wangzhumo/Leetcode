056bd60cfd65f9f98147615625476c4013ad4634
wangzhumo <wangzhumoo@gmail.com>
2019-07-29 13:01:28 +0800

    [feature]finsh

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@
 #include "yuv_play_sdl.h"
 
 int main(int argc, char *args[]) {
-
-	yuv_player("/Users/wangzhumo/Develop/Media/out.yuv");
+   	yuv_player("/Users/wangzhumo/Develop/Media/out.yuv");
     return 0;
 }

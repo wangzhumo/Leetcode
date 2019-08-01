55ed9e43b7e9f1ccf7be5699e36b08e0165020cb
wangzhumo <wangzhumoo@gmail.com>
2019-08-01 23:51:54 +0800

    [fixbug]fix runtime excption - 2

--- src/play/media_player.c
+++ src/play/media_player.c
@@ -5,6 +5,45 @@
 #include "media_player.h"
 
 
-int play(char *path){
+int play(char *path) {
+
+    SDL_Event event;
+    VideoState *state = NULL;
+    SDL_Window *p_sdl_window;
+    SDL_Renderer *p_sdl_renderer;
+    SDL_Texture *p_sdl_texture;
+
+    //init VideoState
+    state = av_malloc(sizeof(VideoState));
+    av_strlcpy(state->filename, path, sizeof(state->filename));
+    //register all
+    av_register_all();
+
+    //由于MACOS必须在主线程创建 SDL_window
+    if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_TIMER)) {
+        fprintf(stderr, "Could not initialize SDL - %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    //create window
+    p_sdl_window = SDL_CreateWindow(
+            "Wangzhumo's Player",
+            0, 0,
+            640, 480,
+            SDL_WINDOW_RESIZABLE
+    );
+
+    if(!p_sdl_window) {
+        fprintf(stderr, "SDL: could not set video mode:%s - exiting.\n", SDL_GetError());
+        exit(1);
+    }
+
+    //create renderer
+    p_sdl_renderer = SDL_CreateRenderer(p_sdl_window, -1, 0);
+    if(!p_sdl_renderer) {
+        fprintf(stderr, "SDL: could not create renderer.\n");
+        exit(1);
+    }
+
 
 }

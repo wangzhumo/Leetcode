ee111a8e975728fc52c03b01d567681d48226dd4
wangzhumo <wangzhumoo@gmail.com>
2019-08-01 23:38:59 +0800

    [feature]create media_player

---
+++ src/play/media_player.c
@@ -0,0 +1,10 @@
+//
+// Created by 王诛魔 on 2019/8/1.
+//
+
+#include "media_player.h"
+
+
+int play(char *path){
+
+}
---
+++ src/play/media_player.h
@@ -0,0 +1,11 @@
+//
+// Created by 王诛魔 on 2019/8/1.
+//
+
+#ifndef MEDIA_MEDIA_PLAYER_H
+#define MEDIA_MEDIA_PLAYER_H
+
+#endif //MEDIA_MEDIA_PLAYER_H
+
+
+int play(char *path);

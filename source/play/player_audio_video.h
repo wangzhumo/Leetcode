2de4b482d04777ad3a5213285d6a0f9485633d8e
wangzhumo <wangzhumoo@gmail.com>
2019-11-18 15:23:48 +0800

    [feature]change dir

--- source/play/player_audio_video.h
+++ source/play/player_audio_video.h
@@ -5,8 +5,8 @@
 #define MEDIA_PLAYER_AUDIO_VIDEO_H
 
 #include <stdio.h>
-#include "media_helper.h"
 #include <assert.h>
+#include "media_helper.h"
 
 #endif //MEDIA_PLAYER_AUDIO_VIDEO_H
 
--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
 //
 // Created by 王诛魔 on 2019/7/26.
 //
-#include "play/media_player.h"
 
 int main(int argc, char *args[]) {
-    play("/Users/wangzhumo/Develop/Media/orginstory_sombra.mp4");
+
     //play("/Users/phyooos/Media/overwatch.mp4");
     return 0;
 }
